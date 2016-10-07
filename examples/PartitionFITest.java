9e6f084a9d8feb8c102dc09eb6a1ac473f06c440
David Baldwin <baldwdav@oregonstate.edu>
2016-10-07 11:07:23 -0700

    -

--- examples/PartitionFITest.java
+++ examples/PartitionFITest.java
@@ -12,17 +12,20 @@ public class PartitionFITest  {
           Just use properties of the domains without considering the
           actual function under test
 
+
+     B1 - arr = null, toFind = *
+     B2 - arr = any array where toFind exists, toFind = *
+        B2_1 - arr = any array where toFind == arr[0], toFind = *
+            B2_1_1 - arr =  any array where toFind == arr[0], all are elements distinct 
+            B2_2_1 - arr =  any array where toFind == arr[0], there are duplicates
+        B2_2 - arr = any array where toFind == arr[arr.length - 1], toFind = *
+        B2_3 - arr = any array where toFind == arr[i], i!=0 && i!=(arr.length - 1), toFind = *
+     B3 - arr = any array where toFind does not exist, toFind = *
     */
-    // B1 - arr = null, toFind = *
-    // B2 - arr = any array where toFind exists, toFind = *
-    //    B2_1 - arr = any array where toFind == arr[0], toFind = *
-    //       B2_1_1 - arr =  any array where toFind == arr[0], all are elements distinct 
-    //       B2_2_1 - arr =  any array where toFind == arr[0], there are duplicates
-    //    B2_2 - arr = any array where toFind == arr[arr.length - 1], toFind = *
-    //    B2_3 - arr = any array where toFind == arr[i], i!=0 && i!=(arr.length - 1), toFind = *
-    // B3 - arr = any array where toFind does not exist, toFind = *
+
     @Test
     public void testFindIntegerArrNull(){
+	// B_1
 	boolean hit = false;
 	try{
 	    WarmUp.findInteger(null, 27);
@@ -34,20 +37,40 @@ public class PartitionFITest  {
 	assertTrue("Expected an NullPointerException",hit);
     }
     
+
+
+
+
+
+
     @Test
     public void testFindIntegerToFindFirst(){
+    //       B2_1_1 - arr =  any array where toFind == arr[0], all are elements distinct 
 	int[] testArr = new int[10];
 	for(int i=0;i<10;i++){
 	    testArr[i] = i;
 	}
-	//	int result = WarmUp.findInteger(testArr,testArr[0]);
-	//assertTrue(result == 0);
+	int result = WarmUp.findInteger(testArr,testArr[0]);
+	assertEquals(0,result);
+    }
+    
+    
+
+    @Test
+    public void testFindIntegerToFindFirstNonUnique(){
+//       B2_2_1 - arr =  any array where toFind == arr[0], there are duplicates
+	int[] testArr = new int[10];
+	for(int i=0;i<10;i++){
+	    testArr[i] = 0;
+	}
+	int result = WarmUp.findInteger(testArr,testArr[0]);
 	assertEquals(0,result);
     }
     
 
     @Test
     public void testFindIntegerToFindLast(){
+	//    B2_2 - arr = any array where toFind == arr[arr.length - 1], toFind = *
 	int[] testArr = new int[10];
 	for(int i=0;i<10;i++){
 	    testArr[i] = i;
@@ -55,9 +78,23 @@ public class PartitionFITest  {
 	int result = WarmUp.findInteger(testArr,testArr[testArr.length-1]);
 	assertEquals(testArr.length-1,result);
     }
+
+
+    @Test
+    public void testFindIntegerMiddle(){
+	//    B2_2 - arr = any array where toFind == arr[arr.length - 1], toFind = *
+	int[] testArr = new int[10];
+	for(int i=0;i<10;i++){
+	    testArr[i] = i;
+	}
+	int result = WarmUp.findInteger(testArr,testArr[3]);
+	assertEquals(testArr.length-1,result);
+    }
+
     
     @Test
     public void testFindIntegerNotExists(){
+	// B3
 	int[] testArr = new int[10];
 	for(int i=0;i<10;i++){
 	    testArr[i] = i;
@@ -67,56 +104,4 @@ public class PartitionFITest  {
     }
     
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public void testWithBadSize(int n) {
-	int[] a = new int[n];
-	for(int i=0;i<n;i++){
-	    a[i] = i;
-	}
-	CardCollection c = new CardCollection();
-	c.add(Card.newDeck());
-	c.permute(a);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testTooSmall() {
-	testWithBadSize(1);
-    }
-    @Test(expected = IllegalArgumentException.class)
-    public void testTooLarge() {
-	testWithBadSize(100);
-    }
-
-    /*
-    @Test
-    public void testCardRemoveWithDupe(){
-	CardCollection c = new CardCollection();
-	Card c1 = new Card(Card.Face.ACE,Card.Suit.HEART);
-	Card c2 = new Card(Card.Face.TWO,Card.Suit.SPADE);
-	Card c3 = new Card(Card.Face.ACE,Card.Suit.HEART);
-	c.add(c1);
-	c.add(c2);
-	c.add(c3);
-	c.discardCard(2);  // remove last card
-	assertEquals(c1,c.getCards().get(0)); // should be Ace of hearts
-	assertEquals(c2,c.getCards().get(1)); // should be Two of spade
-
-    }
-
-*/
 }
