3d64a477bafa8b3275e41058cb515b47a6b13a0f
David Baldwin <baldwdav@oregonstate.edu>
2016-10-10 12:29:18 -0700

    Partition update from 2nd video

--- examples/PartitionFITest.java
+++ examples/PartitionFITest.java
@@ -12,6 +12,67 @@ public class PartitionFITest  {
           Just use properties of the domains without considering the
           actual function under test
 
+int abs(int x){
+   if(x < 0) return -x;
+   else      return  x;
+}
+       B_1       B_2
+x      <0         >=0
+
+       B_1       B_2    B_3 
+x      <0        0      >0
+
+
+
+
+
+
+
+Attempt #1)
+              B_1      B_2     
+arr           null     non-null
+toFind        <=0        >1       
+
+findInteger( null, 0 )
+findInteger( null, 1 )
+findInteger( ... (non-null), 0 )
+findInteger( ... (non-null), 1 )
+
+
+Attempt #2)
+              B_1      B_2                     B_3
+arr           null     non-null, length == 1   non-null, length > 1
+toFind        <0       =0                      >=1                   
+
+
+
+
+Functionality-based approach
+ 
+    Develops characteristics from a behavioral view of the program under test
+    Harder to develop—requires more design effort
+    May result in better tests, or fewer tests that are as effective
+
+
+Attempt #1
+
+     B1 - arr = null, toFind = *
+     B2 - arr = any array where toFind exists, toFind = *
+     B3 - arr = any array where toFind does not exist, toFind = *
+
+Attempt #2
+
+     B1 - arr = null, toFind = *
+     B2 - arr = any array where toFind exists, toFind = *
+        B2_1 - arr = any array where toFind == arr[0], toFind = *
+        B2_2 - arr = any array where toFind == arr[arr.length - 1], toFind = *
+        B2_3 - arr = any array where toFind == arr[i], i!=0 && i!=(arr.length - 1), toFind = *
+     B3 - arr = any array where toFind does not exist, toFind = *
+
+
+
+Attempt #3
+
 
      B1 - arr = null, toFind = *
      B2 - arr = any array where toFind exists, toFind = *
