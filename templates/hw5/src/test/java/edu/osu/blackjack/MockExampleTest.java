23af5d7419f474d8ec12f429f0d133cfc79c87b7
David Baldwin <baldwdav@oregonstate.edu>
2016-10-26 12:32:04 -0700

    Adding example back to JUnit search path

---
+++ templates/hw5/src/test/java/edu/osu/blackjack/MockExampleTest.java
@@ -0,0 +1,31 @@
+package edu.osu.blackjack;
+
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.Random;
+import static org.mockito.Mockito.*;
+
+public class MockExampleTest {
+    @Test
+    public void testMock(){
+	// Does it run without errors for a simple case?
+	DealerAction dt = mock(DealerAction.class);
+	PlayerAction pa = mock(PlayerAction.class);
+
+	//  This player is feeling charitiable....
+	when(pa.getAction()).thenReturn(PlayerAction.ActionType.HIT)
+                            .thenReturn(PlayerAction.ActionType.HIT)
+                            .thenReturn(PlayerAction.ActionType.HIT)
+                            .thenReturn(PlayerAction.ActionType.HIT)
+                            .thenReturn(PlayerAction.ActionType.HIT)
+                            .thenReturn(PlayerAction.ActionType.HIT)	
+	                    .thenReturn(PlayerAction.ActionType.HIT)	   
+	                    .thenReturn(PlayerAction.ActionType.STAND);	                          
+	SimpleBlackjack j = new SimpleBlackjack(dt,new PlayerAction[]{pa});
+	j.playRound();
+	verify(dt, times(9)).dealCard(pa);
+    }
+	
+	
+}
